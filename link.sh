b4b029e76818022c6c168c52f4021311e54c6487
Paul Hutchings <36493057+studioph@users.noreply.github.com>
2022-09-18 22:58:11 -0400

    Initial setup

---
+++ link.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function link(){
+    local dotfile="$1"
+    ln --symbolic --force "$PWD/$dotfile" "$HOME/$dotfile"
+}
+
+link .bashrc
+link .exports
+link .path
+link .gitconfig
